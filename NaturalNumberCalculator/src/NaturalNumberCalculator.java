cf3366e3b8c4409883f99daa92390f427090fcbb
mifye <100733929+mifye@users.noreply.github.com>
2022-03-08 11:18:34 -0600

    Update NaturalNumberCalculator.java

--- NaturalNumberCalculator/src/NaturalNumberCalculator.java
+++ NaturalNumberCalculator/src/NaturalNumberCalculator.java
@@ -13,7 +13,7 @@
  * division replaces the bottom operand with the quotient and the top operand
  * with the remainder.
  *
- * @author Bruce W. Weide
+ * @author Micah Casey-Fusco
  *
  */
 public final class NaturalNumberCalculator {
